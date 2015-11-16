1338a9446704f4522aac4790e9f02954ad6c7721
James Reese <jreese42.vt.edu>
2015-11-15 20:00:43 -0500

    Implement basic logger class

---
+++ logger.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class Logger:
+    fd = -1
+
+    def __init__(self, fd):
+        """Initialize a logger instance which will write to fd"""
+        self.fd = fd    
+
+    def logDebug(self, text):
+        """Print a debug message containing text"""
+        time = datetime.now().strftime("%H:%M:%D ")
+        self.log(time + "(DBG):\t", text)
+
+    def logWarning(self, text):
+        """Print a debug message containing text"""
+        time = datetime.now().strftime("%H:%M:%D ")
+        self.log(time + "(WARN):\t", text)
+
+    def logError(self, text):
+        """Print a debug message containing text"""
+        time = datetime.now().strftime("%H:%M:%D ")
+        self.log(time + "(ERR):\t", text)
+
+
+
+    def log(self, prefix, text):
+        """Print a log message with a prefix"""
+        if self.fd > -1:
+            self.fd.write(prefix + text + "\n")
+        
